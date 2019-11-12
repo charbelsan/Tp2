0932e15739e94a821c1a1754de391b318d86d990
Utilisateur <user@debian.arcahe.ovh>
2019-11-12 21:15:56 +0100

    POM

---
+++ TP2FINAL/compte/src/main/java/fr/uvsq/amine_charbel/compte/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.amine_charbel.compte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
